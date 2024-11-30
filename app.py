60db006404ba5815fe34e26a2b7c3d3019c77b28
Yusuf Riduan <muhammad.yusuf.riduan@student.mmu.edu.my>
2024-11-30 23:57:57 +0800

    Added dropdown options, javascript for carousel pictures and bare function add stops button (WIP)

--- app.py
+++ app.py
@@ -71,7 +71,14 @@ def automatedEmail(issue, username):
 @app.route('/')
 def home():
     if "user" in session and session["user"] != "":
-        return render_template("index.html", profile_Name = session["user"])
+        # Read options from the .txt file
+        try:
+            with open('static/airport_lists.txt', 'r') as file:
+                options = [line.strip() for line in file if line.strip()]  # Remove empty lines
+        except FileNotFoundError:
+            options = []
+
+        return render_template("index.html", profile_Name = session["user"], options=options)
     else:
         return redirect(url_for("register"))
 
